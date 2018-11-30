c3b74decfa0552d02e51c038918908499ab5094d
redspear <willkoia@gmail.com>
2018-11-30 15:47:59 +1000

    Added android version of module

--- android/src/main/java/com/reactlibrary/RNCharisDecodePackage.java
+++ android/src/main/java/com/reactlibrary/RNCharisDecodePackage.java
@@ -12,17 +12,13 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 public class RNCharisDecodePackage implements ReactPackage {
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNCharisDecodeModule(reactContext));
-    }
-
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
 
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      return Arrays.<NativeModule>asList(new RNCharisDecodeModule(reactContext));
     }
+
 }
